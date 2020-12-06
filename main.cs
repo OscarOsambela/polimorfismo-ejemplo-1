d44c42d37371c8566e9f63908bf4cf85a2c300e3
Oscar Michel Michel <osambelacolareta@gmail.com>
2020-12-06 20:16:08 +0000

    add code

--- main.cs
+++ main.cs
@@ -8,15 +8,25 @@ class MainClass {
   BMW bmw = new BMW("A4", 2000, "negro");
   Audi audi = new Audi("M3", 2500, "gris");
   M4 m4 = new M4("M4", 2500, "verde");
-
+  
   auto.MostrarDetalles();
   auto.Reparar();
+  auto.SetearAutoInfo(23400, "Oscar Osambela");
+  auto.LeerAutoInfo();
+
   bmw.MostrarDetalles();
   bmw.Reparar();
+  bmw.SetearAutoInfo(0000123, "Oscar Osambela");
+  bmw.LeerAutoInfo();
+
   audi.MostrarDetalles();
   audi.Reparar();
-	m4.MostrarDetalles();
+  audi.SetearAutoInfo(567328, "Francisco");
+  audi.LeerAutoInfo();
+
+  m4.MostrarDetalles();
   m4.Reparar();
+  m4.SetearAutoInfo(34001, "Vernita");
 
   Console.WriteLine("=========================");
 
@@ -37,6 +47,9 @@ class Auto{
   public int HP{get;set;}
   public string COLOR{get;set;}
 
+  //tiene relacion con clase AutoInfo
+  protected AutoInfo autoinfo = new AutoInfo();
+
   public Auto(int hp, string color){
     this.HP = hp;
     this.COLOR = color;
@@ -46,6 +59,16 @@ class Auto{
   }
   public virtual void Reparar(){
     Console.WriteLine("Auto reparado");
+  }
+
+  //metodos setter y getter relacionados a clase autoinfo
+  public void SetearAutoInfo(int id, string propietario){
+    //no necesita this porque es una propiedad que proviene de otra clase
+     autoinfo.ID = id;
+     autoinfo.PROPIETARIO = propietario;
+  }
+  public void LeerAutoInfo(){
+    Console.WriteLine("El Id {0} del auto es de {1}.", autoinfo.ID, autoinfo.PROPIETARIO);
   }
 }
 
@@ -58,7 +81,7 @@ class BMW:Auto{
   public override void MostrarDetalles(){
     Console.WriteLine("Modelo: {0}, Marca: {1}, HP: {2}, Color: {3}", MODELO, marca, HP, COLOR);
   }
-   public sealed override void Reparar(){
+   public override void Reparar(){
     Console.WriteLine("El BWM {0} está reparado", MODELO);
   }
 }
@@ -78,8 +101,13 @@ class Audi:Auto{
 }
 
 class M4:BMW{
+  public int PUERTAS{get;set;}
 	public M4(string modelo, int hp, string color):base(modelo, hp, color){}
 }
 
+class AutoInfo{
+  public int ID{get;set;} = 0;
+  public string PROPIETARIO{get;set;} = "No tiene propietario";
+}
 //Nota.-
 //solo se puede sellar metodos que han sido rescritos
